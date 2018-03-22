3f1bc3ddd5bcaf05be00568e69e18dc101ed9f77
Nambi <mnambi@amazon.com>
2018-03-22 19:04:12 -0400

    Added CMake example.

---
+++ CMake/wee/wee.cpp
@@ -0,0 +1,16 @@
+#ifndef SOMELIBRARY_H
+#define SOMELIBRARY_H
+
+#include <iostream>
+
+class Wee
+{
+    public:
+    void print()
+        {
+            std::cout << "SomeLibrary: print()" << std::endl;
+        }
+
+};
+
+#endif // SOMELIBRARY_H
